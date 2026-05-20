415606a44a4eda3a67805c68841492b400e6f6e2
Sourav Das <sourav177official@gmail.com>
2026-05-20 22:04:10 +0530

    contact page fix

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,11 +1,33 @@
-from fastapi import APIRouter,Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Request, Form
 from app.routes.home import templates
+import smtplib
+from email.message import EmailMessage
+router = APIRouter()
 
-router=APIRouter()
+@router.post("/contact/mail")
+def mailsend(request: Request, name: str = Form(...), email: str = Form(...), message: str = Form(...)):
+    email_sender = "sourav177official@gmail.com"
+    email_password = "jffd fskx tbxl syjh"
 
+    email_receiver = "sourav177official@gmail.com"
+    msg = EmailMessage()
+    msg["Subject"] = f"A email form {name}"
+    msg["From"] = email_sender
+    msg["To"] = email_receiver
+    body=f"""
+    Hi
+    I am {name}
+    Email : {email}
+    message : {message}
+"""
+    msg.set_content(body)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.send_message(msg)
+
+    return templates.TemplateResponse("maildone.html", {"request": request})
 
 @router.get("/contact")
-def contect(request:Request):
-    if request.method=="GET":
-        return templates.TemplateResponse(request,'contact.html')
+def contect(request: Request):
+    return templates.TemplateResponse(request, 'contact.html')
