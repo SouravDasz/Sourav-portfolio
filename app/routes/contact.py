b4c6d7e46cb38026627d432c1f36349e7c8a5aab
Sourav Das <sourav177official@gmail.com>
2026-05-21 21:46:38 +0530

    fix email mechanizm

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -4,34 +4,34 @@ import smtplib
 from email.message import EmailMessage
 router = APIRouter()
 
-@router.post("/contact/mail")
-def mailsend(request: Request, name: str = Form(...), email: str = Form(...), message: str = Form(...)):
-    email_sender = "sourav177official@gmail.com"
-    email_password = "jffd fskx tbxl syjh"
+# @router.post("/contact/mail")
+# def mailsend(request: Request, name: str = Form(...), email: str = Form(...), message: str = Form(...)):
+#     email_sender = "sourav177official@gmail.com"
+#     email_password = "jffd fskx tbxl syjh"
 
-    email_receiver = "sourav177official@gmail.com"
-    msg = EmailMessage()
-    msg["Subject"] = f"A email form {name}"
-    msg["From"] = email_sender
-    msg["To"] = email_receiver
-    body=f"""
-    Hi
-    I am {name}
-    Email : {email}
-    message : {message}
-"""
-    msg.set_content(body)
+#     email_receiver = "sourav177official@gmail.com"
+#     msg = EmailMessage()
+#     msg["Subject"] = f"A email form {name}"
+#     msg["From"] = email_sender
+#     msg["To"] = email_receiver
+#     body=f"""
+#     Hi
+#     I am {name}
+#     Email : {email}
+#     message : {message}
+# """
+#     msg.set_content(body)
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+#     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
 
-        smtp.starttls()
+#         smtp.starttls()
 
-        smtp.login(email_sender, email_password)
+#         smtp.login(email_sender, email_password)
 
-        smtp.send_message(msg)
+#         smtp.send_message(msg)
 
-    return templates.TemplateResponse("maildone.html", {"request": request})
+#     return templates.TemplateResponse("maildone.html", {"request": request})
 
-@router.get("/contact")
-def contect(request: Request):
-    return templates.TemplateResponse(request, 'contact.html')
+# @router.get("/contact")
+# def contect(request: Request):
+#     return templates.TemplateResponse(request, 'contact.html')
